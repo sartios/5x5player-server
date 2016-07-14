b5f1c20eb9fbf1119be4b16af199cd0f31fdf96a
sartios <sartios@hotmail.com>
2016-07-14 12:36:36 +0300

    add players list

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+var players = require('./app_server/routes/players');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/players', players);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
