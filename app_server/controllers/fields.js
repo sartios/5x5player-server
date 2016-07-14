335649399902bc025190de61082bfce871bf0df4
sartios <sartios@hotmail.com>
2016-07-14 14:40:06 +0300

    add create fields view

--- app_server/controllers/fields.js
+++ app_server/controllers/fields.js
@@ -1,3 +1,10 @@
 module.exports.listFields = function(req, res){
     res.render('field/fields',{});
 };
+
+module.exports.viewCreateField = function(req, res){
+    res.render('field/create-field');
+};
+module.exports.createField = function(req, res){
+    res.render('field/fields');
+};
--- app_server/routes/fields.js
+++ app_server/routes/fields.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var ctrlFields = require('../controllers/fields');
 
 router.get('/', ctrlFields.listFields);
+router.get('/new', ctrlFields.viewCreateField);
+router.get('/create-field', ctrlFields.createField);
 
 module.exports = router;
