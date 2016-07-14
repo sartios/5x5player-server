7a5112907edb29a562dd35f1b43c66d166119d7c
sartios <sartios@hotmail.com>
2016-07-14 15:00:30 +0300

    add create team view

--- app_server/controllers/teams.js
+++ app_server/controllers/teams.js
@@ -1,3 +1,11 @@
 module.exports.listTeams = function(req, res){
     res.render('team/teams', {});
 };
+
+module.exports.viewCreateTeam = function(req, res){
+    res.render('team/create-team');
+};
+
+module.exports.createTeam = function(req, res){
+    res.render('team/teams');
+};
--- app_server/routes/teams.js
+++ app_server/routes/teams.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var ctrlTeams = require('../controllers/teams');
 
 router.get('/', ctrlTeams.listTeams);
+router.get('/new', ctrlTeams.viewCreateTeam);
+router.get('/create-team', ctrlTeams.createTeam);
 
 module.exports = router;
