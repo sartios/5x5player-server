979ca9cd66471e3b60e1a942cb45701837921430
sartios <sartios@hotmail.com>
2016-07-14 14:28:38 +0300

    add create players view

--- app_server/controllers/players.js
+++ app_server/controllers/players.js
@@ -1,3 +1,11 @@
 module.exports.listPlayers = function(req, res){
   res.render('player/players',{title: 'Players'});
 };
+
+module.exports.viewCreatePlayer = function(req, res){
+  res.render('player/create-player');
+};
+
+module.exports.createPlayer = function(req, res){
+  res.render('player/players');
+};
--- app_server/routes/players.js
+++ app_server/routes/players.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var ctrlPlayers = require('../controllers/players');
 
 router.get('/', ctrlPlayers.listPlayers);
+router.get('/new', ctrlPlayers.viewCreatePlayer);
+router.get('/create-player', ctrlPlayers.createPlayer);
 
 module.exports = router;
