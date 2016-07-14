8180165a8bc1dbb85d6ec0312155a0b3a11fcdd8
sartios <sartios@hotmail.com>
2016-07-14 12:56:37 +0300

    add fields list

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
 var players = require('./app_server/routes/players');
 var teams = require('./app_server/routes/teams');
+var fields = require('./app_server/routes/fields');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/players', players);
 app.use('/teams', teams);
+app.use('/fields', fields);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
